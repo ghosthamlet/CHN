5805d536968318bbaff767332273254429aad7e2
ghosthamlet <ghosthamlet@gmail.com>
2019-08-17 23:23:21 +0800

    refactor

--- react.py
+++ react.py
@@ -3,8 +3,27 @@
 import urwid
 
 
+_APP = None
+_ROOT_EL = urwid.Pile([])
+
+
 class Component:
-    def __init__(self):
+    instances = {}
+
+    @classmethod
+    def factory(cls, instance_name, **props):
+        obj = Component.instances.get(instance_name)
+
+        if obj is not None:
+            obj.props = props
+        else:
+            obj = cls(**props)
+            Component.instances[instance_name] = obj
+
+        return obj
+
+    def __init__(self, **props):
+        self.props = props
         self.state = {}
 
     def set_state(self, m, 
@@ -13,15 +32,18 @@ class Component:
             self.state[k] = v
 
         if not disable_render:
-            React.root_el.contents = self.render()
+            # TODO: just render self
+            # self.contents = self.render()
+            if _APP is not None:
+                _ROOT_EL.contents = _APP.render()
 
     def render(self):
         pass
 
 
 class React:
-    root_el = urwid.Pile([])
-
     def render(app):
-        React.root_el.contents = app.render()
-        urwid.MainLoop(React.root_el, palette=[('reversed', 'standout', '')]).run()
+        global _APP
+        _APP = app
+        _ROOT_EL.contents = app.render()
+        urwid.MainLoop(_ROOT_EL, palette=[('reversed', 'standout', '')]).run()
--- ui.py
+++ ui.py
@@ -1,12 +1,36 @@
 #coding=utf8
 
+import sys
 import re
+import logging
 
 import urwid
 
 from chn import HnData, HnCrawler, HnAnalyze
 
 from react import React, Component 
+
+
+logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+logger = logging.getLogger()
+
+fileHandler = logging.FileHandler("{0}/{1}.log".format('data', 'ui'))
+fileHandler.setFormatter(logFormatter)
+logger.addHandler(fileHandler)
+
+consoleHandler = logging.StreamHandler()
+consoleHandler.setFormatter(logFormatter)
+logger.addHandler(consoleHandler)
+logger.setLevel(logging.INFO)
+# logger.info('')
+
+# logging.basicConfig(
+#     level=logging.INFO,
+#     format="%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s",
+#     handlers=[
+#         logging.FileHandler("{0}/{1}.log".format(logPath, fileName)),
+#         logging.StreamHandler()
+#     ])
 
 
 class HnButton(urwid.Button):
@@ -40,80 +64,14 @@ def exit_program(button):
     raise urwid.ExitMainLoop()
 
 
-class HnConsole(Component):
-    def __init__(self):
-        self.hn_data = HnData()
-        self.crawle  = HnCrawler()
-        self.analyze = HnAnalyze()
+class LoginForm(Component):
+    def __init__(self, **props):
+        super().__init__(**props)
 
         self.state = dict(
-                current_page_type='hot',
-                current_cat='All',
                 username='',
                 password='',
-                is_login=False,
-                posts={},
         )
-
-        self.init()
-
-    def init(self, crawler_new=False):
-        if crawler_new:
-            self.crawle.save('/', self.state['current_page_type'])
-        
-        self.load_posts(self.state['current_page_type'])
-
-    def render_header(self):
-        posts = {}
-        for _, p in self.get_posts(self.state['current_page_type']).items():
-            if p['cat'] not in posts:
-                posts[p['cat']] = 1
-            else:
-                posts[p['cat']] += 1
-        posts = sorted(posts.items(), key=lambda x: x[1], reverse=True)
-
-        choices = []
-        # for cat in self.analyze.model.classes_[:6]:
-        for cat, cnt in posts[:6]:
-            if cnt > 0:
-                choices.append('%s(%s)' % (cat, cnt))
-        header_el = urwid.Padding(self.render_menu(choices), left=1, right=1)
-        return header_el
-
-    def load_posts(self, page_type):
-        self.hn_data.load_posts(page_type)
-        posts = self.hn_data.all_posts[page_type]
-        self.analyze.assoc_cat(posts)
-
-        self.set_state({'all_posts': {page_type: posts}})
-
-    def get_posts(self, page_type):
-        return self.state['all_posts'][page_type]
-
-    def render_posts(self, posts):
-        choices = []
-        posts_sorted = sorted(posts.items(), key=lambda x: x[1]['rank'])
-
-        for url, p in posts_sorted:
-            choices.append('%s. [%s] %s' % (p['rank'], p['cat'], p['title']))
-        ls_el = urwid.Padding(create_list(choices), left=1, right=1)
-        return ls_el
-
-    def render_menu(self, choices):
-        body = []
-        choices = ['All', *choices]
-        for c in choices:
-            button = urwid.Button(c)
-            urwid.connect_signal(button, 'click', lambda el, choice: self.on_select_cat(choice), c)
-            body.append(urwid.AttrMap(button, None, focus_map='reversed'))
-
-        focus_el = urwid.SimpleFocusListWalker(body)
-        # have to wrap Columns/GridFlow in ListBox, or Pile can't work
-        return urwid.ListBox([urwid.Columns(focus_el)])
-
-    def on_select_cat(self, choice):
-        key = re.sub('\(.+\)', '', choice)
-        self.set_state({'current_cat': key})
 
     def render_login_form(self):
         username_el = urwid.Edit('Username: ')
@@ -134,14 +92,27 @@ class HnConsole(Component):
             return
 
         # self.crawle.login(self.state['username'], self.state['password'])
-        self.set_state({'is_login': True})
+        self.props['on_login']()
 
-    def render_page_btns(self):
+    def render(self):
+        if self.props['is_login']:
+            login_el = urwid.ListBox([urwid.Text(self.state['username'])])
+        else:
+            login_el = self.render_login_form()
+
+        return login_el
+
+
+class PageBtns(Component):
+    def on_select_page(self, page_alias):
+        pass
+
+    def render(self):
         body = []
         choices = []
 
-        for page_alias, meta in self.hn_data.pages.items():
-            if not self.state['is_login'] and meta['login']:
+        for page_alias, meta in self.props['pages'].items():
+            if not self.props['is_login'] and meta['login']:
                 continue
 
             button = urwid.Button(page_alias)
@@ -152,30 +123,113 @@ class HnConsole(Component):
         # have to wrap Columns/GridFlow in ListBox, or Pile can't work
         return urwid.ListBox([urwid.Columns(focus_el)])
 
-    def on_select_page(self, page_alias):
-        pass
+
+class Header(Component):
+    def render_menu(self, choices):
+        body = []
+        choices = ['All', *choices]
+        for c in choices:
+            button = urwid.Button(c)
+            urwid.connect_signal(button, 'click', lambda el, choice: self.on_select_cat(choice), c)
+            body.append(urwid.AttrMap(button, None, focus_map='reversed'))
+
+        focus_el = urwid.SimpleFocusListWalker(body)
+        # have to wrap Columns/GridFlow in ListBox, or Pile can't work
+        return urwid.ListBox([urwid.Columns(focus_el)])
+
+    def on_select_cat(self, choice):
+        key = re.sub('\(.+\)', '', choice)
+        self.props['on_select_cat'](key)
 
     def render(self):
-        if self.state['is_login']:
-            login_el = urwid.ListBox([urwid.Text(self.state['username'])])
-        else:
-            login_el = self.render_login_form()
-        pages_el = self.render_page_btns()
-        header_el = self.render_header()
-        posts = {url:p for url, p in self.get_posts(self.state['current_page_type']).items()
-                 if p['cat'] == self.state['current_cat'] or self.state['current_cat'] == 'All'}
-        ls_el = self.render_posts(posts)
+        posts = {}
+        for _, p in self.props['posts'].items():
+            if p['cat'] not in posts:
+                posts[p['cat']] = 1
+            else:
+                posts[p['cat']] += 1
+        posts = sorted(posts.items(), key=lambda x: x[1], reverse=True)
 
-        # page = urwid.Frame(ls_el, header_el, focus_part='header')
+        choices = []
+        # for cat in self.analyze.model.classes_[:6]:
+        for cat, cnt in posts[:6]:
+            if cnt > 0:
+                choices.append('%s(%s)' % (cat, cnt))
+        header_el = urwid.Padding(self.render_menu(choices), left=1, right=1)
+        return header_el
+
+
+class Posts(Component):
+    def render(self):
+        choices = []
+        posts_sorted = sorted(self.props['posts'].items(), key=lambda x: x[1]['rank'])
+
+        for url, p in posts_sorted:
+            choices.append('%s. [%s] %s' % (p['rank'], p['cat'], p['title']))
+        return urwid.Padding(create_list(choices), left=1, right=1)
+
+
+class App(Component):
+    def __init__(self, **props):
+        super().__init__(**props)
+
+        self.hn_data = HnData()
+        self.crawle  = HnCrawler()
+        self.analyze = HnAnalyze()
+
+        self.state = dict(
+                current_page_type='hot',
+                current_cat='All',
+                is_login=False,
+                posts={},
+        )
+
+        self.init()
+
+    def init(self, crawler_new=False):
+        if crawler_new:
+            self.crawle.save('/', self.state['current_page_type'])
+        
+        self.load_posts(self.state['current_page_type'])
+
+    def load_posts(self, page_type):
+        self.hn_data.load_posts(page_type)
+        posts = self.hn_data.all_posts[page_type]
+        self.analyze.assoc_cat(posts)
+
+        self.set_state({'all_posts': {page_type: posts}})
+
+    def get_posts(self, page_type):
+        return self.state['all_posts'][page_type]
+
+    def on_login(self):
+        self.set_state({'is_login': True})
+
+    def on_select_cat(self, cat):
+        self.set_state({'current_cat': cat})
+
+    def render(self):
+        is_login = self.state['is_login']
+        current_cat = self.state['current_cat']
+        posts = self.get_posts(self.state['current_page_type'])
+        posts_filtered = {url:p for url, p in posts.items()
+                          if p['cat'] == current_cat or current_cat == 'All'}
+
+        login_el = LoginForm.factory('login', is_login=is_login, on_login=self.on_login).render()
+        page_btns_el = PageBtns.factory('page_btns', is_login=is_login, pages=self.hn_data.pages).render()
+        header_el = Header.factory('header', posts=posts, on_select_cat=self.on_select_cat).render()
+        posts_el = Posts.factory('posts', posts=posts_filtered).render()
+
+        # page = urwid.Frame(posts_el, header_el, focus_part='header')
         return [
             (login_el, ('weight', 1)), 
-            (pages_el, ('weight', 1)), 
+            (page_btns_el, ('weight', 1)), 
             (header_el, ('weight', 2)),
-            (ls_el, ('weight', 8))
+            (posts_el, ('weight', 8))
         ]
 
 
 if __name__ == '__main__':
     from train import *
-    React.render(HnConsole())
+    React.render(App())
 
